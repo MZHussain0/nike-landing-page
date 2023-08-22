e0c0d143d32f091b92b02ae300df91ce99e6f676
zak <mzhussain092@gmail.com>
2023-08-22 18:08:43 +0530

    project init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">Popular Products</section>
+      <section className="padding">Super quality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">Special offers</section>
+      <section className="bg-pale-blue padding ">Cusotmer reviews</section>
+      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+      <section className="bg-black w-full padding-x padding-t pb-8">
+        Footer
+      </section>
+    </main>
+  );
+}
+
+export default App;
